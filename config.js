3f97a1a7c5d53a5edb6bafa2ee7045289c826f57
Javaboymr2k <javaboymr2k@gmail.com>
2024-10-17 15:20:07 +0200

    Update config.js

--- config.js
+++ config.js
@@ -21,23 +21,23 @@ DEBUG = process.env.DEBUG === undefined ? false : convertToBool(process.env.DEBU
 module.exports = {
   HANDLERS: (process.env.PREFIX || '^[.,!]').trim(),
   BRANCH: "main",
-  MODE: (process.env.MODE || 'private').toLowerCase(),
+  MODE: (process.env.MODE || 'public').toLowerCase(),
   ERROR_MSG: toBool(process.env.ERROR_MSG) || true,
   LOG_MSG: toBool(process.env.LOG_MSG) || true,
   READ_CMD: toBool(process.env.READ_CMD),
-  SESSION_ID: process.env.SESSION_ID || "Session id",
+  SESSION_ID: process.env.SESSION_ID || "izumi~pWSs2Mnv",
   MENU_URL: process.env.MENU_URL || "https://ik.imagekit.io/eypz/1722873079279_lHOJlrddC.png",
   CAPTION: process.env.CAPTION || "Iᴢᴜᴍɪ",
   READ_MSG: toBool(process.env.READ_MSG),
-  OWNER_NAME: process.env.OWNER_NAME || "Eypz God",
-  BOT_NAME: process.env.BOT_NAME || "𝚰𝚭𝐔𝚳𝚰-𝚅3",
-  SUDO: process.env.SUDO || null,
+  OWNER_NAME: process.env.OWNER_NAME || "JAVA BOY MR2K TECH",
+  BOT_NAME: process.env.BOT_NAME || "MR2K TECH BOT",
+  SUDO: process.env.SUDO || 260967838144,
   LANG: process.env.LANGUAGE === undefined ? 'EN' : process.env.LANGUAGE.toUpperCase(),
   STICKER_PACKNAME: process.env.STICKER_PACKNAME || "Izumi-v3,❤️",
   AUDIO_DATA: process.env.AUDIO_DATA || "Eʏᴘᴢ;Iᴢᴜᴍɪ-ᴠ3;https://i.imgur.com/cO0TZJv.jpeg",
   PROCESSNAME: process.env.PROCESSNAME || "Izumi-v3",
-  AUTHOR: process.env.AUTHOR || "Eypz God",
-  DELETED_LOG_CHAT: process.env.DELETED_LOG_CHAT || false,
+  AUTHOR: process.env.AUTHOR || "JAVA BOY MR2K TECH ",
+  DELETED_LOG_CHAT: process.env.DELETED_LOG_CHAT || true,
   HEROKU_APP_NAME: process.env.HEROKU_APP_NAME || "",
   HEROKU_API_KEY: process.env.HEROKU_API_KEY || "",
   KOYEB_API_KEY: process.env.KOYEB_API_KEY || "your_koyeb_api_key",
@@ -46,7 +46,8 @@ module.exports = {
   HEROKU: toBool(process.env.HEROKU) || false,
   TERMUX: toBool(process.env.TERMUX) || false,
   DATABASE_URL: DATABASE_URL,
-  DATABASE:
-       DATABASE_URL === './database.db' ? new Sequelize({dialect: 'sqlite', storage: DATABASE_URL, logging: false,}) : new Sequelize(DATABASE_URL, {dialect: 'postgres', ssl: true, protocol: 'postgres', dialectOptions: {native: true, ssl: { require: true, rejectUnauthorized: false },}, logging: false,}),
+  DATABASE 
+AUTO_STATUS_VIEW = true" > config.env 
+      DATABASE_URL === './database.db' ? new Sequelize({dialect: 'sqlite', storage: DATABASE_URL, logging: false,}) : new Sequelize(DATABASE_URL, {dialect: 'postgres', ssl: true, protocol: 'postgres', dialectOptions: {native: true, ssl: { require: true, rejectUnauthorized: false },}, logging: false,}),
   DEBUG: DEBUG
 };
